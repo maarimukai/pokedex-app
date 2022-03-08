5f4264fe3ffcea4d44271ef5b6661f45695b0372
Mariane Mukai <maaarimukai@gmail.com>
2022-03-08 17:05:04 -0300

    Add templete and style

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
-  <h1>POKEDEX</h1>
+  <body class="bkg-img">
+      <h1>POKEDEX</h1> 
+      <form>
+        <input class="ipt-txt" type="text">
+        <input class="ipt-btn" type="submit" value="search">
+      </form>
+  </body>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
 
@@ -11,12 +17,47 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap');
+  #app {
+    font-family: 'Montserrat', sans-serif;
+    background-color: #E06666;
+    background-image: url(https://www.itl.cat/pngfile/big/293-2935792_implied-pokeball-wallpaper-by-giovanni-cotonecoda-fur-circle.png);
+    background-position: center;
+    width: 100vw;
+    height: 100vh;
+  }
+
+  body {
+    margin: 0;
+  }
+
+  h1 {
+    margin-top: 0;
+    padding-top: 50px;
+    text-align: center;
+    font-size: 5rem;
+    color: whitesmoke;
+  }
+
+  form {
+    text-align: center;
+    padding-top: 5rem;
+  }
+
+  .ipt-txt {
+    width: 20vw;
+    padding: 10px 15px 10px 15px;
+    font-size: 1rem;
+    border-radius: 5px;
+    border: none;
+  }
+
+  .ipt-btn {
+    width: 7vw;
+    padding: 10px 15px 10px 15px;
+    font-size: 1rem;
+    color: darkgray;
+
+  }
+
 </style>
